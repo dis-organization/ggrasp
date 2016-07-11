b164b71b6b085c4541528bdb7ccf84fe09c582d5
Michael Sumner <mdsumner@gmail.com>
2016-07-11 15:54:07 +1000

    update data

--- data-raw/holy_poly.R
+++ data-raw/holy_poly.R
@@ -1,8 +1,14 @@
-library(manifoldr)
-#devtools::install_github("mdsumner/manifoldr")
+# #devtools::install_github("mdsumner/manifoldr")
+#
+# library(manifoldr)
+# drawing <- DrawingA("data-raw/branched.map", "Drawing")
+# library(spbabel)
+# holy_poly <- sptable(drawing)
+# devtools::use_data(holy_poly, compress = "bzip2")
 
-library(manifoldr)
-drawing <- DrawingA("data-raw/branched.map", "Drawing")
-library(spbabel)
-holy_poly <- sptable(drawing)
-devtools::use_data(holy_poly, compress = "bzip2")
+
+library(rgdal)
+drawing <- readOGR("data-raw", "mybranch")
+ library(spbabel)
+ holy_poly <- sptable(drawing)
+ devtools::use_data(holy_poly, compress = "bzip2")
